6fcf7ff64f52b701020152117726b5d6cababb61
SalmaAaras <salma.aaras.alasbahi@accenture.com>
2026-09-14 17:48:51 +0200

    feat(api): reader module with stream and html content delivery

--- apps/api/src/reader/reader.module.ts
+++ apps/api/src/reader/reader.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DriveModule } from '../drive/drive.module';
 import { ReaderController } from './reader.controller';
 import { ReaderService } from './reader.service';
 
 @Module({
+  imports: [DriveModule],
   controllers: [ReaderController],
   providers: [ReaderService],
   exports: [ReaderService],
